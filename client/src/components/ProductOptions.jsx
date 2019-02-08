515d5cb4c03f464f9cd8d782c1af16b37be7c310
Jake Ascher <ascherj@gmail.com>
2019-02-08 17:44:48 -0500

    Complete webpack and babel configuration, set up server, and create basic React component

---
+++ client/src/components/ProductOptions.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class ProductOptions extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>Hello world!</div>
+    );
+  }
+}
+export default ProductOptions;

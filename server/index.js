fa37dea31b553ba154e0c766d61a69a84e818597
Jake Ascher <ascherj@gmail.com>
2019-02-08 19:22:53 -0500

    Retrieve all products in the db upon GET request to /products

--- server/index.js
+++ server/index.js
@@ -11,6 +11,23 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/../client/dist'));
 
+app.get('/products', (req, res) => {
+  const getProducts = (callback) => {
+    return Product.find((err, products) => {
+      if (err) callback(err, null);
+      callback(null, products);
+    });
+  };
+
+  getProducts((err, products) => {
+    if (err) {
+      console.error('There was an error retrieving the products:', err);
+      res.status(500).send('');
+    }
+    res.status(200).send(JSON.stringify(products));
+  })
+});
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });

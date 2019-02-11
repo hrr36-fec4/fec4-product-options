e34a8b1991d790bb83270820777270585c45699c
Jake Ascher <ascherj@gmail.com>
2019-02-11 13:32:12 -0500

    Load random product upon refresh and display basic product info

--- client/src/components/ProductOptions.jsx
+++ client/src/components/ProductOptions.jsx
@@ -1,14 +1,46 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class ProductOptions extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      product: {},
+      variant: {}
+    };
+
+    this.getRandomProduct = this.getRandomProduct.bind(this);
   }
+
+  componentDidMount() {
+    this.getRandomProduct();
+  }
+
+  getRandomProduct() {
+    axios.get('http://localhost:3001/products/random')
+      .then((response) => {
+
+        let randomVariantIndex = Math.floor(Math.random() * response.data.variants.length);
+
+        this.setState({
+          product: response.data,
+          variant: response.data.variants[randomVariantIndex]
+        });
+      });
+  }
+
   render() {
     return (
-      <div>Hello world!</div>
+      <div className="productOptions">
+        <div className="brand"><a href="#">{this.state.product.brand}</a></div>
+        <div className="title">{this.state.product.title}</div>
+        <div className="itemId">Item #{this.state.product.itemId}</div>
+        <div className="price">${this.state.variant.price}</div>
+        <div className="freeShipping">
+          <a href="#"><i class="fas fa-truck"></i>This item ships for FREE!</a>
+        </div>
+      </div>
     );
   }
 }

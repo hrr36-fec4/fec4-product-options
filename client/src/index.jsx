212a5e3a25de4c5fac8afaa3717137207ff09cd7
Jake Ascher <ascherj@gmail.com>
2019-02-13 18:34:53 -0500

    Update app container <div> id

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ProductOptions from './components/ProductOptions';
 
-ReactDOM.render(<ProductOptions />, document.getElementById('product-options'));
+ReactDOM.render(<ProductOptions />, document.getElementById('app'));

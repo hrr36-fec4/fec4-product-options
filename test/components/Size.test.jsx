8650c500a92440ae00f0050f10f40e92a587dc9f
Jake Ascher <ascherj@gmail.com>
2019-02-14 12:41:56 -0500

    Update 'describe' descriptions

--- test/components/Size.test.jsx
+++ test/components/Size.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Size from '../../client/src/components/Size';
 
-describe('<ProductOptions />', () => {
+describe('<Size />', () => {
   test('Should exist', () => {
     const wrapper = shallow(<Size />);
     expect(wrapper.exists('option')).toBe(true);

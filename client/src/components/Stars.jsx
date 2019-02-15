12b4794dcef522bc3bdb0ba2aa4578de0406e803
Jake Ascher <ascherj@gmail.com>
2019-02-15 17:07:43 -0500

    Add half stars to product rating

--- client/src/components/Stars.jsx
+++ client/src/components/Stars.jsx
@@ -3,15 +3,18 @@ import PropTypes from 'prop-types';
 
 const Stars = ({ rating }) => {
   let num = Math.floor(rating);
+  const decimal = rating - num;
   const stars = [];
 
   for (let i = 0; i < 5; i++) {
     if (num > 0) {
       stars.push(<i className="fas fa-star" key={i} />);
-      num -= 1;
+    } else if (num === 0 && decimal >= 0.5) {
+      stars.push(<i className="fas fa-star-half-alt" key={i} />);
     } else {
       stars.push(<i className="far fa-star" key={i} />);
     }
+    num -= 1;
   }
 
   return <div className="stars">{stars}</div>;

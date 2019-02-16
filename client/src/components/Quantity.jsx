cddd5641e43f34428c09040e98910384af58027f
Jake Ascher <ascherj@gmail.com>
2019-02-15 21:29:35 -0500

    Adjust spacing between elements and make minor style tweaks

--- client/src/components/Quantity.jsx
+++ client/src/components/Quantity.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 const Quantity = () => (
   <div className="quantity-wrapper">
-    Quantity
-    <button type="button">-</button>
-    <input id="quantity-input" />
-    <button type="button">+</button>
+    <label htmlFor="quantity-input">
+      <div className="quantity-label">Quantity</div>
+      <button type="button">
+        <i className="fas fa-minus" />
+      </button>
+      <input id="quantity-input" type="text" placeholder="1" />
+      <button type="button">
+        <i className="fas fa-plus" />
+      </button>
+    </label>
   </div>
 );
 

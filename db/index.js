16e17986472d55e9ad252e6b71cf207fc65f6c50
Jake Ascher <ascherj@gmail.com>
2019-02-19 18:58:16 -0500

    Update database name

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoUri = 'mongodb://localhost/hrei';
+const mongoUri = 'mongodb://localhost/hrei-product-options';
 
 mongoose.connect(mongoUri, { useNewUrlParser: true });
 const db = mongoose.connection;

a19966715ee0a0ec9727b9a56acd9a9bad62df9c
Jake Ascher <ascherj@gmail.com>
2019-02-20 18:00:30 -0500

    Update ReactDOM.render()

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ProductOptions from './components/ProductOptions';
 
-ReactDOM.render(<ProductOptions />, document.getElementById('app'));
+ReactDOM.render(<ProductOptions />, document.getElementById('product-options'));

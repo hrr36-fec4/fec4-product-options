42fd7d99b14ab4a5c5e80050b5f304ee9c0f2b29
Jake Ascher <ascherj@gmail.com>
2019-02-23 16:01:08 -0500

    Update mongoUri again

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoUri = 'mongodb+srv://Administrator:5k%9aD1Gw8pG@cluster0-sp44m.mongodb.net/test?retryWrites=true/hrei-product-options';
+const mongoUri = 'mongodb://Administrator:5k%9aD1Gw8pG@cluster0-shard-00-00-sp44m.mongodb.net:27017,cluster0-shard-00-01-sp44m.mongodb.net:27017,cluster0-shard-00-02-sp44m.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
 
 mongoose.connect(mongoUri, { useNewUrlParser: true, useCreateIndex: true });
 const db = mongoose.connection;

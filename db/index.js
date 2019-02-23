ba98b70f156d35d10c2593e1d1a3e59eb195d4ad
Jake Ascher <ascherj@gmail.com>
2019-02-23 15:51:05 -0500

    Update mongoUri

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoUri = 'mongodb://localhost/hrei-product-options';
+const mongoUri = 'mongodb+srv://Administrator:5k%9aD1Gw8pG@cluster0-sp44m.mongodb.net/test?retryWrites=true/hrei-product-options';
 
 mongoose.connect(mongoUri, { useNewUrlParser: true, useCreateIndex: true });
 const db = mongoose.connection;

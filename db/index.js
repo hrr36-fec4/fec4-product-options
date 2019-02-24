b2e774b302338248f517fa91186bb06be59bd646
Jake Ascher <ascherj@gmail.com>
2019-02-23 21:18:23 -0500

    Update mongoUri with new password

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoUri = 'mongodb+srv://Administrator:5k%9aD1Gw8pG@cluster0-sp44m.mongodb.net/hrei-product-options?retryWrites=true';
+const mongoUri = 'mongodb+srv://Administrator:admin@cluster0-sp44m.mongodb.net/hrei-product-options?retryWrites=true';
 
 mongoose.connect(mongoUri, { useNewUrlParser: true, useCreateIndex: true });
 const db = mongoose.connection;
